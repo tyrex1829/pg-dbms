7619be3bf89e9fdd7c6b3563cc91502987278784
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-12 00:09:35 +0530

    update function in prisma

--- prisma-orm/src/index.ts
+++ prisma-orm/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -19,4 +19,29 @@ async function insertUser(
   console.log(res);
 }
 
+interface UpdateParams {
+  firstName: string;
+  secondName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, secondName }: UpdateParams
+) {
+  const res = await prisma.user.update({
+    where: {
+      username: username,
+    },
+    data: {
+      firstName,
+      secondName,
+    },
+  });
+  console.log(res);
+}
+
 insertUser("user1", "user1_password", "user1name", "user1lastname");
+updateUser("user1", {
+  firstName: "user1nameee",
+  secondName: "user1lastnameee",
+});
